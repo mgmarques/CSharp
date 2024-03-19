b1e6c9dd5bb577d63f5e68c3f892c427b9acdba0
marcelogomesmarques <marcelo.marques@bairesdev.com>
2024-03-19 08:05:26 -0300

    Calculator UT added

--- calculator/Calculator.Tests/CalculatorTest.cs
+++ calculator/Calculator.Tests/CalculatorTest.cs
@@ -1,26 +1,59 @@
-using Xunit;
-using CalculatorLibrary;
+using Xunit.Abstractions;
 
 namespace Calculator.UnitTests.Services
 {
-    public class PrimeService_IsPrimeShould
+    public class WriterTest
     {
-        private readonly CalculatorLibrary.Calculator _primeService;
+        private readonly TextWriter outputWriter;
+        private readonly string message;
 
-        public PrimeService_IsPrimeShould()
+        public WriterTest(TextWriter outputWriter, string message)
         {
-            _primeService = new CalculatorLibrary.Calculator();
+            this.outputWriter = outputWriter;
+            this.message = message;
+
+        }
+
+        public void PrintMessage()
+        {
+            outputWriter.WriteLine(message);
+        }
+    }
+
+    public class CalculatorTest
+    {
+        private readonly CalculatorLibrary.Calculator _calculator;
+        private readonly ITestOutputHelper output;
+
+        public CalculatorTest(ITestOutputHelper output)
+        {
+            _calculator = new CalculatorLibrary.Calculator();
+            this.output = output;
+
         }
 
         [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        [InlineData(1)]
-        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        [InlineData(2.0, 2.0, "a", 4.0, "Add")]
+        [InlineData(1.0, 1.0, "s", 0.0, "Subtract")]
+        [InlineData(3.0, 2.0, "m", 6.0, "Multiply")]
+        [InlineData(3.0, 2.0, "d", 1.5, "Divide")]
+        [InlineData(3.0, 0.0, "d", double.NaN, "Error: Division by zero is not allowed.")]
+        public void CalculatorDo(double num1, double num2, string op,
+            double expected, string message)
         {
-            var result = _primeService.IsPrime(value);
+            var result = _calculator.DoOperation(num1, num2, op);
 
-            Assert.False(result, $"{value} should not be prime");
+            Assert.Equal(expected, result);
+            output.WriteLine(message);
+
+            var content = new System.Text.StringBuilder();
+            var writer = new StringWriter(content);
+            var sut = new WriterTest(writer, message);
+
+            sut.PrintMessage();
+
+            var actualOutput = content.ToString();
+            Assert.Equal(message + "\n", actualOutput);
         }
     }
 }
